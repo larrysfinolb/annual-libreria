3bf9bc1bdb65eb6fd363e685e5150ce8e8e9c36a
Astridmcf <moleroastridcarolina@gmail.com>
2022-08-27 17:17:20 -0400

    Login finalizado

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,32 +5,25 @@ function Login(root) {
     <main>
         <section>
             <form>
-
             <div class="container w-75 bg-light mt-5 rounded shadow">
             <div class="row align-items-md-stretch">
                 <div class="col bg">
-
             </div>
             <div class="col">
             <div class="text-end">
                 <img src="img/fl.jpg" width="48" alt=""> 
             </div>
-
             <h2 class="fw-bold text-center py-5">Bienvenido</h2>
-
             <!--LOGIN-->
-
             <form action="#">
                 <div class="md-4">
                     <label for ="email" class="form-label">Correo electrónico</label>
                     <input type="email" class="form-control" name="email">
                 </div>
-
                 <div class="md-4">
                     <label for ="password" class="form-label">Contraseña</label>
                     <input type="password" class="form-control" name="email">
                 </div>
-
                 <div class="md-4 form-check">
                     <input type="checkbox" name="connected" class="form-check-input">
                     <label for="connected" class="form-check-label">Mantenerme conectado</label>
@@ -39,7 +32,6 @@ function Login(root) {
                 <div class="d-grid">
                     <button type="submit" class="btn btn-primary">Iniciar Sesión</button>
                 </div>
-
                 <div class="my-3">
                     <span>¿No tienes cuenta? <a href="#">Regístrate</a></span><br>
                     <span><a href="#">Recuperar Contraseña</a></span>
@@ -49,14 +41,11 @@ function Login(root) {
     </div>
 </div>
 		
-
                 
         
             </form>
         </section>
-    </main>    
-    `;
+    </main>        ` ;
 	root.innerHTML = view;
 }
-
 module.exports = Login;
